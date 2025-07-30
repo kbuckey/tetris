de041268feacb95d51f0c9a3ccda4fec39f73de5
lemonzotz <kbuckey12@gmail.com>
2025-07-30 17:42:18 -0600

    beginning javascript code + cleaning div tags (220 -> 200

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () =>{
-
+    const grid = document.querySelector('.grid') 
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
+    const width = 10
 
 
     
