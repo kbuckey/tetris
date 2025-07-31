7ea386fec081874f293e9c28141078152448c831
lemonzotz <kbuckey12@gmail.com>
2025-07-31 00:40:43 -0600

    can now control pieces with left and right arrow keys

--- app.js
+++ app.js
@@ -82,6 +82,21 @@ document.addEventListener('DOMContentLoaded', () =>{
     //moving pieces down the grid
     timerId = setInterval(moveDown, 1000)
 
+    //assign function to keys
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft()
+        } else if (e.keyCode === 38) {
+            //rotate()
+        } else if (e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+
+    document.addEventListener('keyup', control)
+
     function moveDown() {
         erase()
         currentPosition += width
@@ -100,5 +115,30 @@ document.addEventListener('DOMContentLoaded', () =>{
 
         }
     }
+    //move left, but stop shape from moving outside grid
+    function moveLeft() {
+        erase()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
 
+        if(!isAtLeftEdge) currentPosition -= 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1
+        }
+
+        draw()
+    }
+
+    function moveRight() {
+        erase()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+        if(!isAtRightEdge) currentPosition += 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1
+        }
+
+        draw()
+    }
 })
