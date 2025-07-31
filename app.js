a65880a05cc799be8320a9dd1d4ab9298b4ed19e
lemonzotz <kbuckey12@gmail.com>
2025-07-31 14:58:45 -0600

    can now rotate pieces

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ document.addEventListener('DOMContentLoaded', () =>{
         if(e.keyCode === 37) {
             moveLeft()
         } else if (e.keyCode === 38) {
-            //rotate()
+            rotate()
         } else if (e.keyCode === 39) {
             moveRight()
         } else if (e.keyCode === 40) {
@@ -139,6 +139,16 @@ document.addEventListener('DOMContentLoaded', () =>{
             currentPosition -= 1
         }
 
+        draw()
+    }
+
+    function rotate() { 
+        erase()
+        currentRotation++
+        if (currentRotation === current.length) {
+            currentRotation = 0 
+        }
+        current = theShapes[random][currentRotation]
         draw()
     }
 })
